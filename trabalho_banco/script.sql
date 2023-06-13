7855cfc9e23cc5598cf721327da81a1aea2f343f
drws09 <rodrigowerb09@gmail.com>
2023-06-12 22:48:25 -0300

    Subindo Script.sql

--- trabalho_banco/script.sql
+++ trabalho_banco/script.sql
@@ -44,3 +44,49 @@ noc VARCHAR(80), -- 26
 event VARCHAR(60) -- 32
 );
 
+-- 1. Quem são os técnicos (coaches) e atletas das equipes de Handball?
+SELECT CONCAT('Técnico   -   ', name) as `Função  -   Nome` FROM TB_COACHES WHERE discipline = 'Handball'
+UNION
+SELECT CONCAT('Atleta   -   ', name) FROM TB_ATHLETES WHERE discipline = 'Handball';
+
+
+/*
+2. Quem são os técnicos (coaches) dos atletas da Austrália que receberam medalhas de Ouro?
+3. Quais países tiveram mulheres conquistando medalhas de Ouro?
+4. Quais atletas da Noruega receberam medalhas de Ouro ou Prata?
+5. Quais atletas Brasileiros não receberam medalhas?
+ 
+Acredito que não é possível responder essas perguntas por que não há relação dos medalhistas,
+apenas o total de medalhas.
+
+Acredito que seria possível se tivesse por exemplo, uma tabela com as colunas:
+Evento, Medalhista de Ouro, Medalhista de Prata, Medalhista de Bronze
+
+Onde as colunas medalhistas poderiam ser tanto atletas quanto times.
+E para realizar consultas dos atletas que receberam medalhas em esportes em equipe
+fariamos uma query relacionando o noc do atleta com o pais vencedor e a disciplina dos atletas com o evento da tabela
+
+por exemplo essa com a relação dos eventos e os ganhadores das medalhas
+
+CREATE TABLE TB_WINNERS(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+discipline VARCHAR(80),
+gold INT,
+silver INT,
+bronze INT
+);
+
+caso houvesse uma coluna event na tabela atletas (Women - Men - Mixed Team)
+
+ALTER TABLE TB_ATHLETES
+ADD event VARCHAR(60);
+
+poderiamos usar essa query para pegar os atletas que foram medalhistas de ouro pela equipe
+
+select a.ID, a.name from tb_athletes a
+inner join tb_teams t on t.name = a.noc and t.discipline = a.discipline and t.event = a.event
+inner join tb_winners w on t.id = w.gold;
+
+
+mas será que nesse exemplo não quebraria quando um atleta participasse de de eventos individuais e coletivos?
+*/
